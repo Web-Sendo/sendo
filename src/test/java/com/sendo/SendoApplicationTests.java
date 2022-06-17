8b11b41c4124a515299abbc6054fcc8e74e91ba7
TUANANH0 <anhtuan031999@gmail.com>
2022-06-17 10:18:42 +0700

    tao repository

---
+++ src/test/java/com/sendo/SendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
